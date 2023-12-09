a52256cbfa12040e2c4c9b2e4729e4005841f303
minazaher <menazaher115@gmail.com>
2023-12-09 03:18:54 +0200

    get specified user and update methods added

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -12,8 +12,10 @@ object Main {
   implicit val ec : ExecutionContext = ExecutionContext.fromExecutorService(executor)
 
   def main(args: Array[String]): Unit = {
-    insertUser(testUser)
+//    insertUser(testUser)
+    updateUser(1)
     getAllUsers()
+    getUserByName("name")
   }
 
   def insertUser(user: User): Unit ={
@@ -35,5 +37,22 @@ object Main {
     Thread.sleep(10000)
   }
 
+  def getUserByName(username : String): Unit ={
+    val res : Future[Seq[User]] = Connection.db.run(SlickTables.userTable.filter(_.name.like(username)).result) // => select * from users where name like :username
+    res.onComplete{
+      case Success(users) => println(s"Query Successfully Done: $users")
+      case Failure(ex) => println(s"Query Failed Because of : $ex")
+    }
+    Thread.sleep(10000)
+  }
 
+  def updateUser(id :Long): Unit ={
+    val queryDesc = SlickTables.userTable.filter(_.id === id).update(testUser.copy(number = 45120))
+    val futureId : Future[Int] = Connection.db.run(queryDesc)
+    futureId.onComplete{
+      case Success(userNewId) => println(s"Query Successfully Done, new id is: $userNewId")
+      case Failure(ex) => println(s"Query Failed Because of : $ex")
+    }
+    Thread.sleep(10000);
+  }
 }
